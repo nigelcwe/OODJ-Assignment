0b34310109fca6f40d6135e24f79b4e9a367fb7c
Nigel Chan <chanwenigel@gmail.com>
2022-05-16 15:18:15 +0800

    Created Human abstract class

---
+++ com/assignment/src/entities/shared/Human.java
@@ -0,0 +1,46 @@
+package com.assignment.src.entities.shared;
+
+public class Human extends Entity {
+    public String firstName;
+    public String lastName;
+    public String fullName;
+    public String phoneNum;
+    public String email;
+
+    public Human(int id, String firstName, String lastName, String phoneNum, String email) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.fullName = firstName + lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
