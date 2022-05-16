1c509cabaae15d12430bb92c5c29b5fd7614ce41
Nigel Chan <chanwenigel@gmail.com>
2022-05-16 15:17:43 +0800

    Created Entity abstract class

---
+++ com/assignment/src/entities/shared/Entity.java
@@ -0,0 +1,17 @@
+package com.assignment.src.entities.shared;
+
+public abstract class Entity {
+    public int id;
+
+    public Entity(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
