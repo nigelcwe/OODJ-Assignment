c6c5a0b50cd9b718f70c7715a19a65ed0b922d19
Nigel Chan <chanwenigel@gmail.com>
2022-05-17 14:39:10 +0800

    Added complete file paths

--- com/assignment/src/entities/shared/FileSelection.java
+++ com/assignment/src/entities/shared/FileSelection.java
@@ -1,10 +1,10 @@
 package com.assignment.src.entities.shared;
 
 public enum FileSelection {
-    Staff("C:\\Users\\chanw\\repos\\OODJ-Assignment\\com\\assignment\\src\\data\\Staff.txt"),
-    Customer(""),
-    Booking(""),
-    Report("");
+    Booking("C:\\Users\\chanw\\repos\\OODJ-Assignment\\com\\assignment\\src\\data\\Booking.txt"),
+    Customer("C:\\Users\\chanw\\repos\\OODJ-Assignment\\com\\assignment\\src\\data\\Customer.txt"),
+    Report("C:\\Users\\chanw\\repos\\OODJ-Assignment\\com\\assignment\\src\\data\\Report.txt"),
+    Staff("C:\\Users\\chanw\\repos\\OODJ-Assignment\\com\\assignment\\src\\data\\Staff.txt");
 
     private final String value;
 
