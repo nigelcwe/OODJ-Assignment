eaef406637fb700e2c91b13bf762905a4a07c37e
Nigel Chan <chanwenigel@gmail.com>
2022-05-18 01:34:29 +0800

    Updated GymSystem.java

--- com/assignment/src/entities/shared/GymSystem.java
+++ com/assignment/src/entities/shared/GymSystem.java
@@ -5,16 +5,18 @@ import com.assignment.src.entities.customer.CustomerImpl;
 import com.assignment.src.entities.report.ReportImpl;
 import com.assignment.src.entities.staff.StaffImpl;
 
+import java.io.IOException;
+
 public class GymSystem {
     public StaffImpl staff;
     public BookingImpl booking;
     public CustomerImpl customer;
     public ReportImpl report;
 
-    public GymSystem(StaffImpl staff, BookingImpl booking, CustomerImpl customer, ReportImpl report) {
-        this.staff = staff;
-        this.booking = booking;
-        this.customer = customer;
-        this.report = report;
+    public GymSystem() throws IOException {
+        this.staff = new StaffImpl();
+        this.booking = new BookingImpl();
+        this.customer = new CustomerImpl();
+        this.report = new ReportImpl();
     }
 }
