a4f524be4c5085e01881b46da1b6be474e28f0d0
Nigel Chan <chanwenigel@gmail.com>
2022-05-18 01:44:44 +0800

    Completed CustomerPort.java, CustomerImpl.java, Customer.java

--- com/assignment/src/entities/customer/Customer.java
+++ com/assignment/src/entities/customer/Customer.java
@@ -3,7 +3,10 @@ package com.assignment.src.entities.customer;
 import com.assignment.src.entities.shared.Human;
 
 public class Customer extends Human {
-    public Customer(int id, String firstName, String lastName, String phoneNum, String email) {
+    public int bookingId;
+
+    public Customer(int id, String firstName, String lastName, String phoneNum, String email, int bookingId) {
         super(id, firstName, lastName, phoneNum, email);
+        this.bookingId = bookingId;
     }
 }
--- com/assignment/src/entities/customer/CustomerPort.java
+++ com/assignment/src/entities/customer/CustomerPort.java
@@ -1,4 +1,11 @@
 package com.assignment.src.entities.customer;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public interface CustomerPort {
+    ArrayList<String[]> getAllCustomer();
+    Customer getById(int id);
+    int modifyCustomer(Customer customer) throws IOException;
+    int createCustomer(Customer customer) throws IOException;
 }
