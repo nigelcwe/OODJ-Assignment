7914f0477d0f8fab3bfe7f373543f221432f65a7
Nigel Chan <chanwenigel@gmail.com>
2022-05-20 22:45:03 +0800

    Added MonthlyReport.pdf and its respective path in FileSelection.java

--- com/assignment/src/entities/shared/FileSelection.java
+++ com/assignment/src/entities/shared/FileSelection.java
@@ -3,6 +3,7 @@ package com.assignment.src.entities.shared;
 public enum FileSelection {
     Booking("C:\\Users\\chanw\\repos\\OODJ-Assignment\\com\\assignment\\src\\data\\Booking.txt"),
     Customer("C:\\Users\\chanw\\repos\\OODJ-Assignment\\com\\assignment\\src\\data\\Customer.txt"),
+    MonthlyReport("C:\\Users\\chanw\\repos\\OODJ-Assignment\\com\\assignment\\src\\data\\MonthlyReport.pdf"),
     Report("C:\\Users\\chanw\\repos\\OODJ-Assignment\\com\\assignment\\src\\data\\Report.txt"),
     Staff("C:\\Users\\chanw\\repos\\OODJ-Assignment\\com\\assignment\\src\\data\\Staff.txt");
 
